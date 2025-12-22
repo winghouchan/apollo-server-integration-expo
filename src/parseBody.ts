b3e16f45e245097a59134e665a1b57d3a70808ee
Wing-Hou Chan <wing@wii.ng>
2025-12-22 14:24:27 +0000

    test: ignore branch handling non-error object errors in function parsing body from test coverage

--- src/parseBody.ts
+++ src/parseBody.ts
@@ -21,10 +21,18 @@ export default async function parseBody(request: Request) {
        * message (see [documentation][1] and [source][2]) while Apollo Server
        * expects the response to be the error message text (see [tests][3]).
        *
+       * While `request.json` only ever throws error objects ([docs][4]), a
+       * branch for handling values which are not error objects is required
+       * to ensure all code paths return so that TypeScript does not trigger
+       * an error (ts7030) during compilation. The `else` branch is ignored
+       * from test coverage for this reason too.
+       *
        * [1]: https://docs.expo.dev/router/web/api-routes/#error-handling
        * [2]: https://github.com/expo/expo/blob/9d6f808e858f539dd534fb27b838abae1719604f/packages/expo-server/src/runtime/error.ts#L18
        * [3]: https://github.com/apollographql/apollo-server/blob/7be3686ae62fb04bb4d0bfddb465d5e89631d4e4/packages/integration-testsuite/src/httpServerTests.ts#L376-L390
+       * [4]: https://developer.mozilla.org/en-US/docs/Web/API/Request/json#exceptions
        */
+      /* istanbul ignore else */
       if (
         typeof error === 'object' &&
         error !== null &&
